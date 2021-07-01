a6dca392923f7917453641467ed9505a9c20c03a
Govind <govinddhage97@gmail.com>
2021-07-01 13:38:18 +0530

    OTP code

---
+++ app/src/main/java/com/example/otpdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.otpdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mukesh.OnOtpCompletionListener;
+import com.mukesh.OtpView;
+
+public class MainActivity extends AppCompatActivity {
+    private OtpView otpView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        otpView = findViewById(R.id.otp_view);
+        otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp) {
+                Log.d("onOtpCompleted=>", otp);
+
+            }
+        });
+    }
+}
